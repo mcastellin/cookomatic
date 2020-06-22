f184b01a8076a268afbca5f589b30529faccd339
Manuel Castellin <manuel@castellinconsulting.com>
2020-06-22 13:57:11 +0100

    chore: extend Recipe model and basic web navigation
    Now we can save a recipe model to the configured database and navigate to recipe detail page

--- app/models.py
+++ app/models.py
@@ -1,9 +1,13 @@
 from app import db
+from datetime import datetime
 
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), index=True, unique=True)
+    ingredients = db.Column(db.UnicodeText())
+    directions = db.Column(db.UnicodeText())
+    createdts = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def __repr__(self):
         return "<Recipe {}>".format(self.name)
