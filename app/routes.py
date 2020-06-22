9ce6ab9dade97994b2e2df1aca8d674064bbc965
Manuel Castellin <manuel@castellinconsulting.com>
2020-06-22 15:18:29 +0100

    feat: implement forms to save and edit recipes
    Now routes and forms to create and edit recipes are in place

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
-from app import app
+from app import app, db
 from app.models import Recipe
-from flask import render_template
+from app.forms import RecipeForm
+from flask import render_template, flash, redirect
+
+
+def save_recipe(recipe, form):
+    form.populate_obj(recipe)
+    db.session.add(recipe)
+    db.session.commit()
 
 
 @app.route("/")
@@ -14,3 +21,27 @@ def index():
 def recipe(id):
     recipe = Recipe.query.filter_by(id=id).first_or_404()
     return render_template("details.html", title=recipe.name, recipe=recipe)
+
+
+@app.route("/edit/recipe/<id>", methods=["GET", "POST"])
+def edit_recipe(id):
+    recipe = Recipe.query.filter_by(id=id).first_or_404()
+    form = RecipeForm(obj=recipe)
+    if form.validate_on_submit():
+        save_recipe(recipe, form)
+        flash("Recipe {} saved succesfully".format(form.name.data))
+        return redirect("/recipe/{}".format(recipe.id))
+
+    return render_template("edit.html", title="Edit recipe", form=form)
+
+
+@app.route("/new/recipe", methods=["GET", "POST"])
+def new_recipe():
+    form = RecipeForm()
+    if form.validate_on_submit():
+        recipe = Recipe()
+        save_recipe(recipe, form)
+        flash("Recipe {} added successfully".format(form.name.data))
+        return redirect("/index")
+
+    return render_template("edit.html", title="Create recipe", form=form)
