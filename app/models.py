480b2f569dac42213dd3c51d46a27445b10c7126
Manuel Castellin <manuel@castellinconsulting.com>
2020-06-24 14:49:32 +0100

    chore: add User table model for demo purposes
    In order to record a video tutorial I wanted to have an additional table Users in the mysql database.

--- app/models.py
+++ app/models.py
@@ -11,3 +11,14 @@ class Recipe(db.Model):
 
     def __repr__(self):
         return "<Recipe {}>".format(self.name)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.String(256), index=True, unique=True)
+    email = db.Column(db.String(256), index=True, unique=True)
+    name = db.Column(db.String(256), index=True)
+    surname = db.Column(db.String(256), index=True)
+
+    def __rep__(self):
+        return "<User {}>".format(self.uid)
